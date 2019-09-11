72fa39cb182fa57c01a2f9a9f923d863dfc0d5ce
Christa <cmlhc99@gmail.com>
2019-09-12 11:42:43 +1200

    PauseMenuScript

---
+++ GameJam_Piano_Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseCanvas.activeSelf)
+            {
+                pauseCanvas.SetActive(false);
+                Time.timeScale = 1f;
+
+            }
+            else if (!pauseCanvas.activeSelf)
+            {
+                pauseCanvas.SetActive(true);
+                Time.timeScale = 0f;
+            }
+
+        }
+
+
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Resume()
+    {
+        pauseCanvas.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
