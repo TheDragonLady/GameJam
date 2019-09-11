c780136d416aedd314abe800a97a79f8d42a6917
Peter <peterisdew@gmail.com>
2019-09-12 11:48:14 +1200

    Game Manager + Tile Script
    WIP

---
+++ GameJam_Piano_Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    public int playerScore;
+    public int playerLife = 3;
+
+    public GameObject tile;
+    public float tileRange;
+    public float tileFreq;
+
+
+
+    public static GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Setup();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    public void Setup()
+    {
+        Instantiate(player, transform.position, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void SpawnTile()
+    {
+        Instantiate(tile, transform.position, Quaternion.identity);
+    }
+
+    public void hit()
+    {
+        
+    }
+
+    public void Miss()
+    {
+
+    }
+}
---
+++ GameJam_Piano_Game/Assets/Scripts/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    Rigidbody rb;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //hit
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameManager.instance.hit();
+    }
+
+    //miss
+    private void OnTriggerEnter(Collider other)
+    {
+        GameManager.instance.Miss();
+    }
+}
